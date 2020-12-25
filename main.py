1329a4076a2ed9a7a3df50ea14bfe199a1382183
Sean Donoghue <sean.donoghue@pm.me>
2020-12-25 22:04:20 +0000

    Tidied up main.py

--- main.py
+++ main.py
@@ -7,33 +7,23 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix="39!", intents=intents)
 
-### !--- COGS ---! ###
-"""
-cogs = [
-    'catwalk',
-    'duel',
-    'events',
-    'logging',
-    'owner',
-    #'welcome'
-]
-"""
+### !--- EXECUTION CODE ---! ###
+#read cogs list from file
 cogs_txt = open("cogs.txt", "r")
 cogs = cogs_txt.read().splitlines()
 
-### !--- EXECUTION CODE ---! ###
 #load all cogs from cogs[] list
 for cog in cogs:  
     bot.load_extension('cogs.%s' % cog)
     print("Loaded cog: %s" % cog)
 
-#remove ugly default help command
-bot.remove_command('help')
-
 #read token from file
 token_txt = open("token.txt", "r")
 TOKEN = token_txt.read()
 token_txt.close()
+
+#remove ugly default help command
+bot.remove_command('help')
 
 #finally run the bot
 bot.run(TOKEN)
