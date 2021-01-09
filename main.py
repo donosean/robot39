1c050c5404d6a35b7508217da3174c175aeaf0a0
Sean Donoghue <sean.donoghue@pm.me>
2021-01-09 15:11:48 +0000

    Added exception handling to main.py

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
-### !--- IMPORTS ---! ###
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 
-### !--- SETTINGS ---! ###
+#set intents and bot command prefix
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="39!", intents=intents)
 
-### !--- EXECUTION CODE ---! ###
 #read cogs list from file
-cogs_txt = open("cogs.txt", "r")
+try:
+    cogs_txt = open("cogs.txt", "r")
+
+except OSError:
+    print("Error opening cogs.txt for reading, please check this file exists.")
+    raise SystemExit
+
 cogs = cogs_txt.read().splitlines()
+cogs_txt.close()
 
 #load all cogs from cogs[] list
 for cog in cogs:  
-    bot.load_extension('cogs.%s' % cog)
-    print("Loaded cog: %s" % cog)
+    try:
+        bot.load_extension('cogs.%s' % cog)
+        print("Loaded cog: %s" % cog)
+
+    except commands.ExtensionNotFound:
+        print("Cog '%s' could not be found." % cog)
+
+    except commands.ExtensionAlreadyLoaded:
+        print("Cog '%s' is already loaded." % cog)
+
+    except commands.NoEntryPointError:
+        print("Cog '%s' has no setup function." % cog)
+
+    except commands.ExtensionFailed:
+        print("Cog '%s' had a setup function error." % cog)
 
 #read token from file
-token_txt = open("token.txt", "r")
+try:
+    token_txt = open("token.txt", "r")
+
+except OSError:
+    print("Error opening token.txt for reading, please check this file exists.")
+    raise SystemExit
+
 TOKEN = token_txt.read()
 token_txt.close()
 
