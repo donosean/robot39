6c2f67f792fd428c495f046c710edf2370419729
Sean Donoghue <sean.donoghue@pm.me>
2021-01-20 15:13:33 +0000

    Code tidied up and all comments rewritten in owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -17,7 +17,7 @@ class Owner(commands.Cog):
         self.bot = bot
 
     ### !--- METHODS ---! ###
-    # Send the same text to the terminal and context channel/DM
+    # Sends the same text to the terminal and context channel/DM
     async def say(self, ctx, text: str):
         try:
             await ctx.reply(text)
@@ -27,7 +27,7 @@ class Owner(commands.Cog):
         
         print("owner: %s" % text)
 
-    # Reload/load/unload cog depending on given CogAction
+    # Reloads/loads/unloads a cog depending on given CogAction
     async def manage_cog(self, ctx, cog: str, action: CogAction):
         try:
             if action == CogAction.reload:
@@ -57,10 +57,10 @@ class Owner(commands.Cog):
         except commands.ExtensionFailed:
             await self.say(ctx, "Cog '%s' had a setup function error." % cog)
     
-    # Send message to a channel; can be a reply to another
+    # Sends message to a channel; can be a reply to another
     # message if a message ID is provided and reply = True
     async def send(self, ctx, channel: Union[int, discord.TextChannel],
-                   message: str, message_id: int = None, reply: bool = False):
+                   message: str, msg_id: int = None, reply: bool = False):
         # Get channel if only a channel ID is given
         if type(channel) == int:
             channel = self.bot.get_channel(channel)
@@ -76,7 +76,7 @@ class Owner(commands.Cog):
 
             # Get the message from the given message ID and send the reply
             else:
-                reply_msg = await channel.fetch_message(message_id)
+                reply_msg = await channel.fetch_message(msg_id)
                 await self.say(ctx, "Replied to message by %s in %s."
                                % (reply_msg.author, channel.name))
                 await reply_msg.reply(message)
@@ -88,70 +88,66 @@ class Owner(commands.Cog):
             await self.say(ctx, "Missing permissions to send message.")
         
     ### !--- CHECKS & COMMANDS ---! ###
+    # Restricts all commands in this cog to the owner of the bot
     async def cog_check(self, ctx):
-        #all commands are only accessible to the owner of the bot
         if await ctx.bot.is_owner(ctx.author):
             return True
-
         else:
-            raise commands.NotOwner('owner: %s does not own this bot.' % ctx.author)
+            raise commands.NotOwner(
+                'owner: %s does not own this bot.' % ctx.author)
 
-    #unloads then re-loads a cog that has been loaded
+    # Reloads a cog that is already loaded
     @commands.command()
     async def reload_cog(self, ctx, cog: str):
         await self.manage_cog(ctx, cog, CogAction.reload)
 
-    #loads a cog that hasn't been loaded
+    # Loads a cog that hasn't been loaded
     @commands.command()
     async def load_cog(self, ctx, cog: str):
         await self.manage_cog(ctx, cog, CogAction.load)
 
-    #unloads a cog that has been loaded
+    # Unloads a cog that has already been loaded
     @commands.command()
     async def unload_cog(self, ctx, cog: str):
         await self.manage_cog(ctx, cog, CogAction.unload)
     
-    #sends a message to a channel given by channel id/mention
+    # Sends a message to a channel given by channel ID/mention
     @commands.command()
-    async def send_message(self, ctx, channel: Union[int, discord.TextChannel], *msg):
-        #create the message string from what was passed with the command
+    async def send_message(self, ctx, channel: Union[int, discord.TextChannel],
+                           *msg):
+        # Join *msg arguments into one string and pass on to send method
         message = ' '.join(msg)
         await self.send(ctx, channel, message)
     
-    #sends a reply to a message given by channel id/mention and message id
+    # Sends a reply to a message given by channel ID/mention and message ID
     @commands.command()
-    async def send_reply(self, ctx, channel: Union[int, discord.TextChannel], msg_id: int, *msg):
-        #create the message string from what was passed with the command
+    async def send_reply(self, ctx, channel: Union[int, discord.TextChannel],
+                         msg_id: int, *msg):
+        # Join *msg arguments into one string and pass on to send method
         message = ' '.join(msg)
         await self.send(ctx, channel, message, msg_id = msg_id, reply = True)
 
-    #adds a reaction to a message given by channel mention, message id and emoji as string
+    # Adds a reaction to a message given by channel ID/mention,
+    # message ID and emoji as string
     @commands.command()
-    async def add_reaction(self, ctx, channel: Union[int, discord.TextChannel], msg_id: int, react_emoji: str):
-        #fetch channel object if only channel id is given
+    async def add_reaction(self, ctx, channel: Union[int, discord.TextChannel],
+                           msg_id: int, react_emoji: str):
+        # Get channel if only a channel ID is given
         if type(channel) == int:
             channel = self.bot.get_channel(channel)
-        
-        #only continue if valid channel is given
         if not channel:
             await self.say(ctx, "Channel not found.")
             return
 
-        #fetch the message from the given message id
-        react_msg = None #avoids 'possibly unbound' warning
+        # Get the message from the ID and add the reaction
         try:
             react_msg = await channel.fetch_message(msg_id)
-        
+            await react_msg.add_reaction(react_emoji)
+            await self.say(ctx, "Reaction added to message by %s in %s."
+                           % (react_msg.author, channel.name))
+
         except discord.NotFound:
             await self.say(ctx, "Message not found.")
-
-        #finally, add the reaction using the given emoji
-        try:
-            await react_msg.add_reaction(react_emoji)
-            await self.say(ctx, "Reaction added to message by %s in %s." % (react_msg.author, channel.name))
-
-        except discord.HTTPException:
-            await self.say(ctx, "Failed to add reaction due to HTTP exception, or potentially nonexistant emoji.")
 
         except discord.Forbidden:
             await self.say(ctx, "Missing permissions to add reaction.")
@@ -161,6 +157,10 @@ class Owner(commands.Cog):
 
         except discord.InvalidArgument:
             await self.say(ctx, "Invalid emoji parameter given.")
+
+        except discord.HTTPException:
+            await self.say(ctx, "Failed to add reaction due to HTTP exception,"\
+                           " or potentially nonexistant emoji.")
         
 ### !--- SETUP ---! ###
 def setup(bot):
