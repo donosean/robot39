fba286150b4aa1ef01821c5217151c6bbcd341ba
Sean Donoghue <sean.donoghue@pm.me>
2021-01-20 17:15:02 +0000

    Rewrote comments and tidied up code in quotes.py

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -1,58 +1,89 @@
 import discord
 from discord.ext import commands
 
+
 class Quotes(commands.Cog):
 
-    ### !--- INIT ---! ###
     def __init__(self, bot):
         self.bot = bot
         self.staff_roles = ["Secret Police"]
         self.quotes_channel_id = 797182745050087455
 
+    ### !--- METHODS ---! ###
+    # Sends the same text to the terminal and context channel
+    async def say(self, ctx, text: str):
+        try:
+            await ctx.reply(text)
+
+        except discord.Forbidden:
+            print("quotes: Missing permissions to reply.")
+        
+        print("quotes: %s" % text)
+
     ### !--- CHECKS & COMMANDS ---! ###
+    # Restricts all commands in this cog to specific checks
     async def cog_check(self, ctx):
-        #all commands will work for the owner of the bot
-        if ctx.guild == None and ctx.bot.is_owner(ctx.author):
+        # Allow bot owner and server admins
+        if (await ctx.bot.is_owner(ctx.author)
+                or ctx.author.guild_permissions.administrator):
             return True
         
-        #or users with admin permissions in their guild
-        elif ctx.author.guild_permissions.administrator:
-            return True
+        # Also allow users with roles listed in self.staff_roles
+        quotes_channel = self.bot.get_channel(self.quotes_channel_id)
+        staff_roles = [discord.utils.get(quotes_channel.guild.roles, name=role)
+                       for role in self.staff_roles]
 
-        #or users with the named role in self.staff_roles
-        quotes_channel =  self.bot.get_channel(self.quotes_channel_id)
-        staff_roles = [discord.utils.get(quotes_channel.guild.roles, name=role) for role in self.staff_roles]
-
-        if any(role in staff_roles for role in ctx.author.roles):
-            return True
-        else:
-            return False
-
-    @commands.command()
+        return any(role in staff_roles for role in ctx.author.roles)
+    
+    # Posts an embed to the quotes channel, containing a quoted message 
+    # obtained from a channel mention and a message ID
+    @commands.command(name="add_quote", aliases=["quote"])
     @commands.guild_only()
-    async def add_quote(self, ctx, channel_mention: discord.TextChannel, message_id):
-        message = await channel_mention.fetch_message(message_id)
-
-        #check message contains actual text content
-        if len(message.content) > 0:
-            msg_content = message.content
-        else:
-            await ctx.send("Message contains no quotable text content.")
-            print("quotes: Message contains no quotable text content.")
+    async def add_quote(self, ctx, channel_mention: discord.TextChannel,
+                        message_id):
+        # Get the message from the mentioned channel using the message ID
+        try:
+            message = await channel_mention.fetch_message(message_id)
+        
+        except discord.Forbidden:
+            await self.say(ctx, "Missing permissions to fetch message.")
             return
 
+        except discord.NotFound:
+            await self.say(ctx, "Message not found.")
+            return
+        
+        except discord.HTTPException:
+            await self.say(ctx, "Retrieving the message failed.")
+            return
+
+        # Check if message contains actual text content
+        if len(message.content) == 0:
+            await self.say(ctx, "Message contains no quotable text content.")
+            return
+
+        # Date of message creation, formatted like "1 January 2021"
         msg_date = message.created_at.strftime("%d %B %Y")
 
-        #create embed to post in quotes channel
+        # Create embed to post in quotes channel
         embed=discord.Embed(title="Quote", color=0x80ffff)
-        embed.add_field(name=msg_content, value="-- %s, [%s](%s)" % (message.author.mention, msg_date, message.jump_url), inline=False)
         embed.set_thumbnail(url=message.author.avatar_url)
+        embed.add_field(name=message.content, value="-- %s, [%s](%s)"
+                        % (message.author.mention, msg_date, message.jump_url),
+                        inline=False)
 
-        #fetch quotes channel and send embed
-        quotes_channel =  self.bot.get_channel(self.quotes_channel_id)
-        await quotes_channel.send(embed=embed)
-        print("quotes: %s added a quote by %s." % (ctx.author, message.author))
+        # Get quotes channel and send the embed
+        try:
+            quotes_channel = self.bot.get_channel(self.quotes_channel_id)
+            await quotes_channel.send(embed=embed)
+            await self.say(ctx, "Added a quote by %s." % (message.author))
 
-### !--- SETUP ---! ###
+        except discord.Forbidden:
+            await self.say(ctx, "Missing permissions to post quote.")
+
+        except discord.HTTPException:
+            await self.say(ctx, "Posting the quote failed.")
+
+
 def setup(bot):
     bot.add_cog(Quotes(bot))
